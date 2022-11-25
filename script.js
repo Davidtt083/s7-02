6146ae6789880fbe1465ee925aed74ef24ecf84c
Davidtt083 <92117303+Davidtt083@users.noreply.github.com>
2022-11-25 09:24:20 -0600

    cambio

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 $('.btn_nav').click(function() {
   // animate content
   $('.page__style').addClass('animate_content');
@@ -15,38 +17,88 @@ $('.btn_nav').click(function() {
 });
 
 // on click show page after 1500ms
-$('.home_link').click(function() {
+$('.paso1').click(function() {
   setTimeout(function() {
     $('.home').addClass('fadeIn');
   }, 1500);
 });
 
-$('.projects_link').click(function() {
+$('.paso2').click(function() {
   setTimeout(function() {
     $('.projects').addClass('fadeIn');
   }, 1500);
 });
 
-$('.skills_link').click(function() {
+$('.paso3').click(function() {
   setTimeout(function() {
     $('.skills').addClass('fadeIn');
   }, 1500);
 });
 
-$('.about_link').click(function() {
+$('.paso4').click(function() {
   setTimeout(function() {
     $('.about').addClass('fadeIn');
   }, 1500);
 });
 
-$('.contact_link').click(function() {
+$('.paso5').click(function() {
   setTimeout(function() {
     $('.contact').addClass('fadeIn');
   }, 1500);
 });
 
-$('.contact_link2').click(function() {
+$('.paso6').click(function() {
   setTimeout(function() {
     $('.projects2').addClass('fadeIn');
   }, 1500);
 });
+
+$('.final').click(function() {
+  setTimeout(function() {
+    $('.skills2').addClass('fadeIn');
+  }, 1500);
+});
+
+
+
+
+
+function makeResizable(element){
+	if (element && jQuery(element).length){
+		var $el = jQuery(element);
+		var elHeight = $el.outerHeight();
+		var elWidth = $el.outerWidth();
+
+		var $wrapper = $el.parent();
+
+		var starterData = {
+			size: {
+				width: $wrapper.width(),
+				height: $wrapper.height()
+			}
+		}
+		var scale = Math.min(
+			starterData.size.width / $el.outerWidth(),
+			starterData.size.height / $el.outerHeight()
+		);
+		if (scale > 1){
+			scale = 1;
+		}
+		var elMarginBottom = (scale * elHeight) - starterData.size.height;
+		$el.css({
+			transform: "translate3d(-50%, 0, 0) " + "scale(" + scale + ")",
+			'margin-bottom': elMarginBottom
+		});
+	}
+}
+jQuery(document).ready(function() {
+	makeResizable('#very-specific-design');
+});
+jQuery(window).load(function() {
+//	makeResizable('#very-specific-design');
+});
+jQuery(window).resize(function() {
+	makeResizable('#very-specific-design');
+});
+
+
